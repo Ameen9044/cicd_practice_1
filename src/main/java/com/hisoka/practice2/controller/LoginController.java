cd28fe3c58b4c39a75c10d9dac65063128f39b39
Ameen9044 <ameenahmad9044@gmail.com>
2022-11-10 02:00:08 +0530

    import added

--- src/main/java/com/hisoka/practice2/controller/LoginController.java
+++ src/main/java/com/hisoka/practice2/controller/LoginController.java
@@ -1,10 +1,8 @@
 package com.hisoka.practice2.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
