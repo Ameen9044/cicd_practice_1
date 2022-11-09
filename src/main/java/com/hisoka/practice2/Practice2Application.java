5841f1f9202d586a288e776450cdd598b175bd1a
Ameen9044 <ameenahmad9044@gmail.com>
2022-11-10 01:56:29 +0530

    updated java code

--- src/main/java/com/hisoka/practice2/Practice2Application.java
+++ src/main/java/com/hisoka/practice2/Practice2Application.java
@@ -2,12 +2,7 @@ package com.hisoka.practice2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
 @SpringBootApplication
 public class Practice2Application {
 
@@ -15,11 +10,5 @@ public class Practice2Application {
 		SpringApplication.run(Practice2Application.class, args);
 	}
 
-	@RequestMapping("/home")
-	public String displayHomePage(Model model, Authentication authentication) {
-		model.addAttribute("username", authentication.getName());
-		model.addAttribute("roles", authentication.getAuthorities().toString());
-		return "home.html";
-	}
 
 }
--- src/main/java/com/hisoka/practice2/controller/LoginController.java
+++ src/main/java/com/hisoka/practice2/controller/LoginController.java
@@ -6,19 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
+@RestController
 public class LoginController {
     
-
-    @RequestMapping(value="/login",method = {RequestMethod.GET,RequestMethod.POST})
-    public String displayLoginPage(@RequestParam(value="error",required = false) String error, Model model)
-    {
-        String errorMessage=null;
-        if (error!=null)
-        { 
-            errorMessage="Username or password incorrect!!";
-        }
-        model.addAttribute("errorMessage",errorMessage);
-        return "login.html";
-    }
+     @GetMapping("/home")
+     public String hello(){return "Hello From Service on ECS!!";}
 }
